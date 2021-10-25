11217d9f3250e5c36e2ec3b942382bec12dbe7d0
djp952 <djp952@gmail.com>
2021-10-24 23:06:09 -0400

    Enable DPI awareness for the main application

--- src/hdhomeruntray/MainForm.cs
+++ src/hdhomeruntray/MainForm.cs
@@ -27,6 +27,19 @@ namespace hdhomeruntray
 {
 	public partial class MainForm : Form
 	{
+		#region Win32 API Declarations
+		/// <summary>
+		/// Win32 API Declarations
+		/// </summary>
+		private static class NativeMethods
+		{
+			public const int SM_CXSMICON = 49;
+
+			[System.Runtime.InteropServices.DllImport("user32.dll")]
+			public static extern int GetSystemMetrics(int smIndex);
+		}
+		#endregion
+
 		public MainForm()
 		{
 			InitializeComponent();
@@ -34,8 +47,11 @@ namespace hdhomeruntray
 
 		private void button1_Click(object sender, System.EventArgs e)
 		{
+			// remove me; testing DPI support
+			int iconsize = NativeMethods.GetSystemMetrics(NativeMethods.SM_CXSMICON);
+
 			// remove me; exercising the collection
-			DeviceCollection devices = DeviceCollection.Create(DiscoveryMethod.Http);
+			DeviceCollection devices = DeviceCollection.Create(DiscoveryMethod.Broadcast);
 		}
 	}
 }
