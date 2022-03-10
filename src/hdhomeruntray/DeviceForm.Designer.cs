947de229b53b1ab96b38428c527ce8c11bf419ee
djp952 <djp952@gmail.com>
2022-03-10 14:26:26 -0500

    Enable double-buffering on all Form objects

--- src/hdhomeruntray/DeviceForm.Designer.cs
+++ src/hdhomeruntray/DeviceForm.Designer.cs
@@ -47,6 +47,7 @@ namespace zuki.hdhomeruntray
 			this.ClientSize = new System.Drawing.Size(280, 115);
 			this.ControlBox = false;
 			this.Controls.Add(this.m_layoutpanel);
+			this.DoubleBuffered = true;
 			this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.MaximizeBox = false;
